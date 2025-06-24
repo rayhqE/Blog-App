11284b3cfd431b629e30db5913e640b399966ba0
Rayyan <rayhq3@gmail.com>
2025-06-24 07:50:26 +0530

    set up partials in views

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-
-
-
-
-
-
-
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(PORT, () => console.log(`App is listening on Port:${PORT}`));
