4392481d4c1a90b5f675905fc22935af7fdfe67d
Rayyan <rayhq3@gmail.com>
2025-06-26 23:20:57 +0530

    mongoDB setup

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const path = require("path");
 const express = require("express");
-
+const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8000;
+
+mongoose
+  .connect("mongodb://localhost:27017/blogify")
+  .then((e) => console.log("MongoDB Connected"));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
