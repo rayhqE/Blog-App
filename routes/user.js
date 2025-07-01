209358ac63d9d3c2395e5e20788450d84c017d1d
Rayyan <rayhq3@gmail.com>
2025-07-01 14:55:36 +0530

    authentication complete

--- routes/user.js
+++ routes/user.js
@@ -15,13 +15,16 @@ router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   try {
     const token = await User.matchPasswordAndGenerateToken(email, password);
-
     return res.cookie("token", token).redirect("/");
   } catch (error) {
     return res.render("signin", {
       error: "Incorrect Email or PassWord",
     });
   }
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token").redirect("/");
 });
 
 router.post("/signup", async (req, res) => {
